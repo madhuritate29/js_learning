aee80f5dfb6d491baa21cadca6f2ec1ae8f5c1c3
madhuritate29 <bhosalemadhuri2512@gmail.com>
2024-03-18 15:33:57 +0530

    Logical (If, Ifelse and Switch Statement)
    Logical (If, Ifelse and Switch Statement)

---
+++ 06_switchCase.js
@@ -0,0 +1,28 @@
+
+var day=8;
+switch (day) {
+    case 1:
+        console.log("Sun");
+        break;
+    case 2:
+        console.log("Mon");
+        break;
+    case 3:
+        console.log("Tue");
+        break;
+    case 4:
+        console.log("Wed");
+        break;
+    case 5:
+        console.log("Thu");
+         break;
+    case 6:
+        console.log("Fri");
+        break;
+    case 7:
+        console.log("Sat");
+        break;
+    default:
+        console.log(`Invalid inputs....`);
+        break;
+}

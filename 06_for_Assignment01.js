6766db8aad2c625d107d2a07ed50661e1b256536
madhuritate29 <bhosalemadhuri2512@gmail.com>
2024-03-19 15:50:07 +0530

    Assign using for loop
    Assign using for loop

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,36 @@
+
+console.log(`---- WAP to print number from 5 to 15 by incrementing 1 ----`);
+
+for (let i = 5; i <=15 ; i++) {
+    console.log(i); 
+}
+
+console.log(`---- WAP to print number from 50 to 40 by decrementing 1 ----`);
+for (let i = 50; i >=40 ; i--) {
+    console.log(i); 
+}
+
+console.log(`---- WAP to find first 15 odd numbers ----`);
+for (let i = 1; i <=15 ; i+=2) {
+    console.log(i); 
+}
+
+console.log(`---- WAP to find first 10 even numbers ----`);
+for (let i = 0; i <=10 ; i+=2) {
+    console.log(i); 
+}
+
+console.log(`---- WAP to print table of 5 like ->5 10 15 20 25 ..... 50`);
+for (let i = 5; i <=50 ; i+=5) {
+    console.log(i); 
+}
+
+console.log(`---- WAP to print table of 10 like ->10 20 30 40 .... 100`);
+for (let i = 10; i <=100 ; i+=10) {
+    console.log(i); 
+}
+
+console.log(`---- WAP to print table of 10 in reverse order like ->100 90 80 .... 10`);
+for (let i = 100; i >=10 ; i-=10) {
+    console.log(i); 
+}
